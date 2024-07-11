6aa4a2e7e7a9206f6dd7502ac9d2893f05362b4f
binghuo007 <hyxjscn@163.com>
2024-07-11 23:20:46 +0800

    REGULAR COMMIT BY JOPLIN PAGES PUBLISHER(v1.7.1)

--- src/driver/webview/postcss.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-/* eslint-env node */
-module.exports = {
-  plugins: {
-    tailwindcss: { config: path.resolve(__dirname, './tailwind.config.js') },
-    autoprefixer: {},
-  },
-};
